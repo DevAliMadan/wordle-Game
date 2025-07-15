b8bd08a0e43a058afb7bee54200cc4580328cd66
Ali Madan <alimadan4321@gmail.com>
2025-07-15 09:20:01 +0300

    adds a words list in a new js file

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,27 @@
+import { WORDS } from "../scripts/words.js";
+
 function init(){
 
-const gameBoard = document.querySelector('.gameBoard');
+
+
+const gameBoard = document.querySelector('.wordle');
+const keyboard = document.querySelector('#keyboard');
+
+
+const guessNumber = 6;
+let guessesRemaining = guessNumber;
+let currentGuess = [];
+let nextLetter = 0;
+let rightGuessString = WORDS[Math.floor(Math.random() * WORDS.length)];
+console.log([rightGuessString]);
+
+
+
+//functions
+
+
+
+
 
 
 
